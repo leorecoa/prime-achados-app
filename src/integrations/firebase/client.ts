da3b56a4b51e384f88d19e9ece8b918d1a229313
AQUELEBIXO\Bruno <leorecoa1@hotmail.com>
2025-06-07 22:17:58 -0300

    feat: adicionar configuração do Firebase e inicializar o aplicativo

---
+++ src/integrations/firebase/client.ts
@@ -0,0 +1,19 @@
+// firebase/client.ts
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMp0eQpL2f6oW2dWzC4KldMuXs8Ku5RgY",
+  authDomain: "prime-achadinhos.firebaseapp.com",
+  databaseURL: "https://prime-achadinhos-default-rtdb.firebaseio.com",
+  projectId: "prime-achadinhos",
+  storageBucket: "prime-achadinhos.appspot.com",
+  messagingSenderId: "139569093020",
+  appId: "1:139569093020:web:5a0af02f858cac6ae209f1",
+  measurementId: "G-8Z28EEPHPY"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { app, database };
