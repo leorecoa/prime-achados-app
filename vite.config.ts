de82301f23b11c01760b69a28a5b87ecc38aeb8b
AQUELEBIXO\Bruno <leorecoa1@hotmail.com>
2025-06-08 11:09:41 -0300

    fix: corrigir importação do módulo path no vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import * as path from "path";
+// @ts-ignore
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
